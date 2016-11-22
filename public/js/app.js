11186c5ca728fb380bf44ff7454c27b6514f70c4
edrees jalili <edreesjalili@gmail.com>
2016-11-22 01:10:18 -0500

    Loading Json Added Angular Some basic card structure added

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,11 @@
-$(document).foundation()
+(function() {
+    'use strict';
+    angular
+        .module("socialwallApp", [])
+        .controller("cardListController", cardListController);
+})();
+
+function cardListController() {
+    var vm = this;
+    vm.posts = getPosts().items;
+}
