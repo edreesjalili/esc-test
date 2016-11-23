511a4478601631306b813319be568f928a94556d
edrees jalili <edreesjalili@gmail.com>
2016-11-23 06:11:40 -0500

    Added git ignore, started using simple web server (python), started using decklist-angular module for masonry like effect, rebuilding cards

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
 (function() {
     'use strict';
     angular
-        .module("socialwallApp", [])
+        .module("socialwallApp", ["akoenig.deckgrid"])
         .controller("cardListController", cardListController);
 })();
 
 function cardListController() {
     var vm = this;
-    vm.posts = getPosts().items;
+    vm.posts = shuffle(getPosts().items);
+}
+
+function shuffle(o){
+  for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+  return o;
 }
