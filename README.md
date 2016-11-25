e3f9506317555fb87e82ff3e3a57b96abd0ce74f
Hamid Jalili <Hamid Jalili>
2016-11-25 04:12:44 -0500

    Finished filter and load more button.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Simple static webpage by Edrees Jalili
+
+Use a simple HTTP Server to serve files.
+
+(I used python in my case)
+
+python -m SimpleHTTPServer
+
+(127.0.0.1:8000 in a web browser)
--- public/js/app.js
+++ public/js/app.js
@@ -2,39 +2,92 @@
     'use strict';
     angular
         .module("socialwallApp", ["akoenig.deckgrid", "ngSanitize", "linkify"])
+        .filter("postFilter", postFilter)
         .controller("cardListController", CardListController);
 })();
 
-function CardListController() {
-    var vm = this;
-    vm.posts = parseTweets(shuffle(getPosts().items));
-}
-
-function shuffle(o){
-  for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
-}
-
-function parseTweets(tweets) {
-    for(var i = 0; i < tweets.length; i++) {
-        if(tweets[i].service_slug === "instagram") {
-            tweets[i].item_data.caption = parseInstagramMessage(tweets[i]);
-            continue;
-        } else if (tweets[i].service_slug === "manual") continue;
-        //fixes @s
-        tweets[i].item_data.tweet = tweets[i].item_data.tweet.replace(/(@([A-Z]*[a-z]*[0-9]*)*)/g, function(a, b) {
-            return '<span class="red-text">'+b+'</span>';
-        });
-        //fixes #s
-        tweets[i].item_data.tweet = tweets[i].item_data.tweet.replace(/(#([A-Z]*[a-z]*[0-9]*)*)/g, function(a, b) {
-            return '<span class="red-text">'+b+'</span>';
+function postFilter() {
+    return function(posts, filters) {
+        return posts.filter(function(post) {
+            if(filters.length === 0) return true;
+            if(filters.indexOf(post.service_slug) < 0)
+                return false;
+            else return true;
         });
     }
-    return tweets;
 }
 
-function parseInstagramMessage(message) {
-    return message.item_data.caption.replace(/(#([A-Z]*[a-z]*[0-9]*)*)/g, function(a, b) {
-            return '<span class="red-text">'+b+'</span>';
+function CardListController($filter) {
+    var vm = this;
+    vm.filters = [];
+    vm.posts = parseTweets(shuffle(getPosts().items));
+    vm.filteredPosts = $filter('postFilter')(vm.posts, vm.filters);
+    vm.disableLoad = false;
+    vm.visibleFilters = false;
+
+    vm.updateFilters = updateFilters;
+    vm.activeFilter = activeFilter;
+    vm.loadMorePosts = loadMorePosts;
+    vm.toggleFilters = toggleFilters;
+
+    
+    function shuffle(o){
+    for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+    }
+
+    function parseTweets(tweets) {
+        for(var i = 0; i < tweets.length; i++) {
+            if(tweets[i].service_slug === "instagram") {
+                tweets[i].item_data.caption = parseInstagramMessage(tweets[i]);
+                continue;
+            } else if (tweets[i].service_slug === "manual") continue;
+            //fixes @s
+            tweets[i].item_data.tweet = tweets[i].item_data.tweet.replace(/(@([A-Z]*[a-z]*[0-9]*)*)/g, function(a, b) {
+                return '<span class="red-text">'+b+'</span>';
+            });
+            //fixes #s
+            tweets[i].item_data.tweet = tweets[i].item_data.tweet.replace(/(#([A-Z]*[a-z]*[0-9]*)*)/g, function(a, b) {
+                return '<span class="red-text">'+b+'</span>';
+            });
+        }
+        return tweets;
+    }
+
+    function parseInstagramMessage(message) {
+        return message.item_data.caption.replace(/(#([A-Z]*[a-z]*[0-9]*)*)/g, function(a, b) {
+                return '<span class="red-text">'+b+'</span>';
+            });
+    }
+
+    function updateFilters(filterOption) {
+        var i;
+        i = vm.filters.indexOf(filterOption);
+        if (i < 0) vm.filters.push(filterOption);
+        else vm.filters.splice(i,1);
+
+        vm.filteredPosts = $filter('postFilter')(vm.posts, vm.filters);
+    }
+
+    function activeFilter(filterOption) {
+        return (vm.filters.indexOf(filterOption) >= 0) ? true : false;
+    }
+
+    function loadMorePosts() {
+        if (vm.disableLoad) return;
+        vm.disableLoad = true;
+        (parseTweets(shuffle(getPosts().items))).forEach(function(post) {
+            vm.posts.push(post);
         });
+        
+        vm.filteredPosts = (function() {
+            var posts = $filter('postFilter')(vm.posts, vm.filters);
+            vm.disableLoad = false;
+            return posts;
+        })();
+    }
+
+    function toggleFilters() {
+        vm.visibleFilters = !vm.visibleFilters;
+    }
 }
